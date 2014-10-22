45646573b7924418ed38885d97dac675627f44a5
Ukang'a Dickson <ukanga@gmail.com>
2014-10-22 13:38:57 +0300

    UK: PrefixHandler

---
+++ di_sms/handlers/prefix.py
@@ -0,0 +1,39 @@
+import re
+
+from django.utils.translation import ugettext as _
+from rapidsms.contrib.handlers import BaseHandler
+from rapidsms.contrib.handlers.exceptions import HandlerError
+
+
+class PrefixHandler(BaseHandler):
+    prefix = None
+    pattern = r'%(prefix)s\s?(\w+.*?)(?=\s*%(prefix)s|$)'
+
+    def handle(self, question, answer):
+        self.respond(
+            _(u"You responded to question number {}, your answer was \"{}\".")
+            .format(question, answer)
+        )
+
+    @classmethod
+    def _pattern(cls):
+        if not hasattr(cls, "prefix"):
+            raise HandlerError('PrefixHandler must define a prefix')
+
+        if hasattr(cls, "pattern") and cls.pattern and cls.prefix:
+            return re.compile(cls.pattern % {'prefix': cls.prefix},
+                              re.IGNORECASE)
+
+        raise HandlerError('PrefixHandler must define a pattern.')
+
+    @classmethod
+    def dispatch(cls, router, msg):
+        pattern = cls._pattern()
+
+        match = pattern.match(msg.text)
+        if match is None:
+            return False
+
+        cls(router, msg).handle(pattern.findall(msg.text))
+
+        return True
